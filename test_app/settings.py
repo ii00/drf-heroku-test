488e225a9d554dd5185b7b88c5399e8e91e35dc9
btmn <ivayloivanov00@gmail.com>
2022-08-22 23:23:11 +0300

    procfile changed, gunicorn installed

--- test_app/settings.py
+++ test_app/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# import django_on_heroku
-# django_on_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
