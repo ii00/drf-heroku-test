cdf0ea722c642a0b9414ba97cf9d9513f98a965f
btmn <ivayloivanov00@gmail.com>
2022-08-24 05:41:31 +0300

    Frontend (WP: Heroku setup), Backend (WP: Heroku setup)

--- api/views.py
+++ api/views.py
@@ -1,9 +1,5 @@
-from django.http.response import Http404
-from django.shortcuts import render
-from rest_framework.views import APIView
-from .models import Todo
-from .serializers import TodoSerializer
-from rest_framework.response import Response
+from .models import Todo, NotTodo
+from .serializers import TodoSerializer, NotTodoSerializer
 from rest_framework.viewsets import ModelViewSet
 
 class TodoModelViewSet(ModelViewSet):
@@ -11,3 +7,7 @@ class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
+class NotTodoModelViewSet(ModelViewSet):
+    # crud set
+    queryset = NotTodo.objects.all()
+    serializer_class = NotTodoSerializer    
